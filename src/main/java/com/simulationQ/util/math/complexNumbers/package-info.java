cc652c3760f4022ffb8d629c7f652a799fd897a0
Tsvetelin Kostadinov <tsvetelinkostadinovts@gmail.com>
2019-09-29 15:59:47 +0300

    Added utility classes for work with complex numbers

---
+++ src/main/java/com/simulationQ/util/math/complexNumbers/package-info.java
@@ -0,0 +1,9 @@
+/*
+ * 27/09/2019 17:24:16
+ * package-info.java created by Tsvetelin
+ */
+/**
+ * @author Tsvetelin
+ *
+ */
+package com.simulationQ.util.math.complexNumbers;
---
+++ src/main/java/com/simulationQ/util/math/package-info.java
@@ -0,0 +1,9 @@
+/*
+ * 26/09/2019 16:01:37
+ * package-info.java created by Tsvetelin
+ */
+/**
+ * @author Tsvetelin
+ *
+ */
+package com.simulationQ.util.math;
